234d8f1d2287ce3fe68ce8a34492e30e0699de99
Johann G <johann@guinberteau.fr>
2024-04-16 11:38:19 +0200

    ✨ Moved to PHP

---
+++ client/config.php
@@ -0,0 +1,5 @@
+<?php
+
+# Connection to python server
+define("SERVER_HOST", "localhost");
+define("SERVER_PORT", 8765);
--- client/index.php
+++ client/index.php
@@ -1,5 +1,15 @@
+<?php
+
+if (isset($_POST['name'])) {
+    $name = htmlspecialchars($_POST['name']);
+
+
+}
+
+?>
+
 <!doctype html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +25,6 @@
 
     <title>Moto Rapido</title>
 
-    <link rel="stylesheet" href="assets/styles/game.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -30,19 +39,21 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="d-flex justify-content-center">
-                    <label for="name" class="visually-hidden">Nom</label>
-                    <input type="text" class="form-control w-auto m-1" id="name" placeholder="Nom" required>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="d-flex justify-content-center">
-                    <label for="ip" class="visually-hidden">Adresse IP</label>
-                    <input type="text" class="form-control w-auto m-1" id="ip" placeholder="Adresse IP" required>
 
-                    <button class="btn btn-primary m-1" id="connect">Rejoindre
+        <?php if (isset($error)): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Impossible de rejoindre la partie.</strong> <?= $error ?>.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+        <form class="row g-3" action="" method="POST">
+            <div class="d-flex justify-content-center">
+                <div class="col-auto m-1">
+                    <label for="name" class="visually-hidden">Pseudo</label>
+                    <input type="text" class="form-control w-auto" name="name" id="name" placeholder="Pseudo" required>
+                </div>
+                <div class="col-auto m-1">
+                    <button type="submit" class="btn btn-primary">Rejoindre la partie
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                              class="bi bi-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
@@ -51,22 +62,7 @@
                     </button>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
-
-<div id="game" style="display: none;">
-    <div class="d-flex justify-content-center">
-        <img src="assets/img/logo.png" alt="logo" class="img-fluid" width="10%">
-    </div>
-
-    <!-- Players -->
-    <div class="float-start">
-        <div class="text-center moto">
-            <!--            Player -->
-            <img src="assets/img/motos/ktm.png" alt="Joueur 1" width="100%">
-            <p>Joueur 1</p>
-        </div>
+        </form>
     </div>
 </div>
 
