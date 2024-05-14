1763e902a32502bfc72c153830d51c3583f1ff56
Johann G <johann@guinberteau.fr>
2024-05-14 11:35:52 +0200

    🔊 Logs

--- server/cache.py
+++ server/cache.py
@@ -3,27 +3,33 @@ from typing import List, Dict
 
 r = None
 
+
 def connect():
     global r
     r = redis.Redis(host='localhost', port=6379, decode_responses=True)
     if r.ping():
-        print("Connected to Redis.")
+        print("REDIS>> Connected.")
 
 
 def getColors() -> List[str]:
     return r.lrange('colors', 0, -1)
 
+
 def getPlayerColors() -> Dict[str, str]:
     return r.hgetall('players')
+
 
 def getPlayerScores() -> Dict[str, int]:
     return r.zrange('players:leaderboard', 0, -1, withscores=True)
 
+
 def setColors(colors):
     r.delete('colors')
     r.rpush('colors', *colors)
+    print(f"REDIS>> Added {len(colors)} colors. ")
+
 
 def clearPlayers():
     r.delete('players')
     r.delete('players:leaderboard')
-    print("Cleared players")
+    print("REDIS>> Cleared players.")
